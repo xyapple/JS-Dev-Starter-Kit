32b71d070c2c3cc1e986a97503ff769579262f0b
Yin Deascentis <yd@Yins-MBP.fios-router.home>
2017-05-22 11:40:12 -0400

    Update the srcServer.js with express package requirments; Add index.html in the new src folder

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -1,1 +1,18 @@
 var express = require('express');
+var path = require('path');
+var open = require('open');
+
+var port = 3000;
+var app = express();
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '../src/index.html'))
+});
+
+app.listen(port, function(error){
+  if(error){
+    console.error(error);
+  } else{
+    open('http://localhost:' + port);
+  }
+})
