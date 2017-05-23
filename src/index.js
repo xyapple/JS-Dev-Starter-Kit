669f10185b965a6adf71d342558a2bd7d6b8807a
Yin Deascentis <yd@Yins-MBP.fios-router.home>
2017-05-23 11:07:19 -0400

    Without React or other library; This is just Javascript

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import './index.css';
+ import './index.css';
+// import numeral from 'numeral';
+//
+// const courseVale = numeral(1000).format('$0,0.00');
+// // debugger;
+// let c = "apple"
+// console.log(`i will buy ${c} with ${courseVale}`);
 
-import numeral from 'numeral';
+import {getUsers} from './api/userApi';
 
-const courseVale = numeral(1000).format('$0,0.00');
-// debugger;
-let c = "apple"
-console.log(`i will buy ${c} with ${courseVale}`);
+//Poplulate table of users vs
+getUsers().then(result =>{
+    let usersBody = "";
+    result.forEach(user =>{
+      usersBody += `<tr>
+  <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+  <td>${user.id}</td>
+  <td>${user.firstName}</td>
+  <td>${user.lastName}</td>
+  <td>${user.email}</td>
+    </tr>`
+  });
+  
+global.document.getElementById('users').innerHTML = usersBody;
+});
