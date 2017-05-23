174e01812ffffc4ad6848c9d77a173c5fa8ce33b
Yin Deascentis <yd@Yins-MBP.fios-router.home>
2017-05-23 11:10:31 -0400

    Update the html.test file to reference the same JSDOM.

--- src/index.test.js
+++ src/index.test.js
@@ -10,11 +10,11 @@ describe('First test', ()=>{
 });
 
 describe('index.html', () =>{
-  it('should say test', (done)=>{
+  it('should have h1 that says Users', (done)=>{
     const index = fs.readFileSync('./src/index.html', 'utf-8');
     jsdom.env(index, function(err, window){
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal("Test");
+      expect(h1.innerHTML).to.equal("Users");
       done();
       window.close();
     });
