97eb221ea8b58ed558829b89f7dd1305b3d80e07
Yin Deascentis <yd@Yins-MBP.fios-router.home>
2017-05-23 09:17:19 -0400

    complete adding ESlint and eslint-watch

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import './index.css';
 import numeral from 'numeral';
 
 const courseVale = numeral(1000).format('$0,0.00');
-debugger;
+// debugger;
 let c = "apple"
 console.log(`i will buy ${c} with ${courseVale}`);
